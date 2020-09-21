7a03ae40b4cf4672b4a2f4e061d02abd51fd7273
w3qc <mitnik.gong@gmail.com>
2020-09-21 22:21:14 +0800

    first commit

---
+++ pages/index/index.vue
@@ -0,0 +1,33 @@
+<template>
+	<view class="content">
+		<!-- courses -->
+		<uni-list>
+			<uni-list-item v-for="" :key="" title=""></uni-list-item>
+		</uni-list>
+		<!-- classes -->
+		<uni-list>
+			<uni-list-item v-for="" :key="" title=""></uni-list-item>
+		</uni-list>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				myCourses: [], // my-courses-visited-desc
+				myClasses: [], // my-classes-visited-desc
+			}
+		},
+		onLoad() {
+			// fetch my courses and classes
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
